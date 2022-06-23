8a340667d5c6db12978843eb3539fd7c9489b6a8
Trua Shamu <sofia_mit@hotmail.com>
2022-06-23 10:40:33 -0500

    ws

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', adminRoutes);
 
-app.listen(3000);
-
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
